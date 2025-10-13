5f8b7a361032a53742de3d0aa4d57e52332bf6cf
urafiz <kratser88@gmail.com>
2025-10-13 21:33:37 +0200

    test shoret version of intent

--- src/components/ProdDeepLinkPage.js
+++ src/components/ProdDeepLinkPage.js
@@ -3,28 +3,28 @@ import "./ProdDeepLinkPage.css";
 
 function ProdDeepLinkPage() {
   // Detect if user is on Samsung Internet browser
-  const isSamsungBrowser = () => {
-    return /SamsungBrowser/i.test(navigator.userAgent);
-  };
+  // const isSamsungBrowser = () => {
+  //   return /SamsungBrowser/i.test(navigator.userAgent);
+  // };
 
   // Generate appropriate deep link based on browser
-  const generateDeepLink = () => {
-    const httpsUrl = "https://ripple.staging.icanbwell.com";
-    const packageName = "com.thedacare.v2.staging"; // Replace with your actual package name
-    const playStoreUrl = `https://play.google.com/store/apps/details?id=${packageName}`;
+  // const generateDeepLink = () => {
+  //   const httpsUrl = "https://ripple.staging.icanbwell.com";
+  //   const packageName = "com.thedacare.v2.staging"; // Replace with your actual package name
+  //   const playStoreUrl = `https://play.google.com/store/apps/details?id=${packageName}`;
 
-    if (isSamsungBrowser()) {
-      console.log("Samsung Internet browser detected");
-      // Use intent:// URL for Samsung Internet
-      const intentPath = "ripple.staging.icanbwell.com";
-      return `intent://${intentPath}#Intent;scheme=https;package=${packageName};S.browser_fallback_url=${encodeURIComponent(
-        playStoreUrl
-      )};end`;
-    }
+  //   if (isSamsungBrowser()) {
+  //     console.log("Samsung Internet browser detected");
+  //     // Use intent:// URL for Samsung Internet
+  //     const intentPath = "ripple.staging.icanbwell.com";
+  //     return `intent://${intentPath}#Intent;scheme=https;package=${packageName};S.browser_fallback_url=${encodeURIComponent(
+  //       playStoreUrl
+  //     )};end`;
+  //   }
 
-    // Standard HTTPS URL for other browsers (App Links)
-    return httpsUrl;
-  };
+  //   // Standard HTTPS URL for other browsers (App Links)
+  //   return httpsUrl;
+  // };
 
   const handleContinueClick = () => {
     // const deepLink = generateDeepLink();
